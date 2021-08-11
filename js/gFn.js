244cc47c8469f5f06ffceca10f3afd88d8c60ac6
Doonngg <61999334+Doonngg@users.noreply.github.com>
2021-08-11 22:03:13 +0800

    1

--- js/gFn.js
+++ js/gFn.js
@@ -1,12 +1,22 @@
 window.addEventListener("load",function(){
 	let firstPage = document.querySelector(".firstPage");
 	let secondPage = document.querySelector(".secondPage");
+	let personalInformation = document.querySelector("#personal_information");
 	let gSelf = document.querySelector(".gSelf");
+	let leftBtn = document.querySelector(".left_btn");
+	let pageName = document.querySelector(".page_name");
 	//第二页位置大小设置
-	secondPage.style.top = "-" + firstPage.offsetHeight + "px";
+	secondPage.style.top = - firstPage.offsetHeight + "px";
 	secondPage.style.height = firstPage.offsetHeight + "px";
-	gSelf.addEventListener("click",function(){
-		myAnimate(".secondPage", "0", "top", "top .5s");
+	personalInformation.addEventListener("click",function(){
+		secondPage.src = "personal_information.html"
+		myAnimate(".secondPage", "0", "top", "top .3s");
+		myAnimate(".secondPage_head", "0", "top", "top .4s");
+		pageName.innerText = "个人信息";
+	})
+	leftBtn.addEventListener("click",function(){
+		myAnimate(".secondPage", - firstPage.offsetHeight + "px", "top", "top .3s");
+		myAnimate(".secondPage_head", "-56px", "top", "top .4s");
 	})
 	//css3动画封装
 	function myAnimate(animateNode, animateEnd, animateStyle, animateTransition) {
