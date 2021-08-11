04f9c4bd9358284c54c9a11513c7d59160d43a68
Doonngg <61999334+Doonngg@users.noreply.github.com>
2021-08-11 20:30:23 +0800

    2

--- js/gFn.js
+++ js/gFn.js
@@ -1,3 +1,16 @@
 window.addEventListener("load",function(){
-	
+	let firstPage = document.querySelector(".firstPage");
+	let secondPage = document.querySelector(".secondPage");
+	let gSelf = document.querySelector(".gSelf");
+	//第二页位置大小设置
+	secondPage.style.top = "-" + firstPage.offsetHeight + "px";
+	secondPage.style.height = firstPage.offsetHeight + "px";
+	gSelf.addEventListener("click",function(){
+		myAnimate(".secondPage", "0", "top", "top .5s");
+	})
+	//css3动画封装
+	function myAnimate(animateNode, animateEnd, animateStyle, animateTransition) {
+		document.querySelector(animateNode).style.transition = animateTransition;
+		document.querySelector(animateNode).style[animateStyle] = animateEnd;
+	}
 })
