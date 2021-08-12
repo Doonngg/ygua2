31e705f6d9d3c9d11b2ff10f62df7ff1f88f874a
Doonngg <61999334+Doonngg@users.noreply.github.com>
2021-08-12 09:33:26 +0800

    3

--- js/gFn.js
+++ js/gFn.js
@@ -1,18 +1,18 @@
 window.addEventListener("load",function(){
 	let firstPage = document.querySelector(".firstPage");
 	let secondPage = document.querySelector(".secondPage");
-	let personalInformation = document.querySelector("#personal_information");
 	let gSelf = document.querySelector(".gSelf");
 	let leftBtn = document.querySelector(".left_btn");
-	let pageName = document.querySelector(".page_name");
 	//第二页位置大小设置
 	secondPage.style.top = - firstPage.offsetHeight + "px";
 	secondPage.style.height = firstPage.offsetHeight + "px";
-	personalInformation.addEventListener("click",function(){
-		secondPage.src = "personal_information.html"
-		myAnimate(".secondPage", "0", "top", "top .3s");
-		myAnimate(".secondPage_head", "0", "top", "top .4s");
-		pageName.innerText = "个人信息";
+	//个人信息
+	document.querySelector("#personal_information").addEventListener("click",function(){
+		gIframe("personal_information.html","个人信息");
+	})
+	//修改密码
+	document.querySelector("#change_password").addEventListener("click",function(){
+		gIframe("change_password.html","修改密码");
 	})
 	leftBtn.addEventListener("click",function(){
 		myAnimate(".secondPage", - firstPage.offsetHeight + "px", "top", "top .3s");
@@ -22,5 +22,12 @@ window.addEventListener("load",function(){
 	function myAnimate(animateNode, animateEnd, animateStyle, animateTransition) {
 		document.querySelector(animateNode).style.transition = animateTransition;
 		document.querySelector(animateNode).style[animateStyle] = animateEnd;
+	}
+	//iframe事件封装
+	function gIframe(iframeUrl,iframeName){
+		secondPage.src = iframeUrl;
+		myAnimate(".secondPage", "0", "top", "top .3s");
+		myAnimate(".secondPage_head", "0", "top", "top .4s");
+		document.querySelector(".page_name").innerText = iframeName;
 	}
 })
